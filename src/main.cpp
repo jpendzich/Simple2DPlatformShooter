915d0340ed7a4513d67187dbe97e30bffa30d5d5
Juri Pendzich <Juri05@gmx.net>
2025-06-05 10:12:13 +0200

    Build working, basic shape rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/Shape.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/System/Vector2.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({1600, 900}), "CMake SFML Project");
     window.setFramerateLimit(144);
+
+	sf::RectangleShape rect(sf::Vector2f(100, 100));
+	rect.setFillColor(sf::Color(255, 0, 0, 255));
 
     while (window.isOpen())
     {
@@ -15,7 +24,9 @@ int main()
             }
         }
 
-        window.clear();
+        window.clear(sf::Color(255, 255, 255, 255));
+		window.draw(rect);
+
         window.display();
     }
 }
